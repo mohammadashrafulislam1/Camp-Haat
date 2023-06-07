02c525c99718610178079571af064787ba566977
mohammadashrafulislam <ixdell41@gmail.com>
2023-06-07 23:11:02 +0600

    filter email and stop existing users to add on DB

--- src/shared/SocialLogin/SocialLogin.jsx
+++ src/shared/SocialLogin/SocialLogin.jsx
@@ -1,5 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../../hock/useAuth";
+import Swal from "sweetalert2";
 
 const SocialLogin = () => {
     const location = useLocation();
@@ -8,7 +9,34 @@ const SocialLogin = () => {
     const {googleSignIn} = useAuth();
     const handelGoogleLogin = () =>{
         googleSignIn()
-        .then(()=>{
+        .then((result)=>{
+            const loggedUser = result.user;
+            const saveUser = {
+                name: loggedUser.displayName,
+                email: loggedUser.email,
+                role:"Student",
+              };
+            fetch("http://localhost:5000/users", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(saveUser),
+        })
+            .then((res) => res.json())
+            .then((res) => {
+              console.log(res)
+              if (res.insertedId) {
+                Swal.fire({
+                  position: "top-end",
+                  icon: "success",
+                  title: `Successfully created: ${loggedUser.displayName}`,
+                  showConfirmButton: false,
+                  timer: 1500,
+                });
+                navigate('/')
+              }
+            });
             navigate(from)
         })
     }
