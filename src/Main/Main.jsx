47447a4292237ba2c1cefc891520f3deeee9c01d
mohammadashrafulislam <ixdell41@gmail.com>
2023-06-07 13:16:18 +0600

    fixed width of total website

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
     const loginHeaderFooter = location.pathname.includes('login')
     const registerHeaderFooter = location.pathname.includes('register')
     return (
-        <div className="">
+        <div className="max-w-screen-lg mx-auto">
             {loginHeaderFooter || registerHeaderFooter || <Header></Header>}
             <Outlet></Outlet>
             {loginHeaderFooter || registerHeaderFooter || <Footer></Footer>}
