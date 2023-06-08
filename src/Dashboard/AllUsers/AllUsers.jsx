22bb2b13e739cc451ba4a02b32d4e9cbe8400d56
mohammadashrafulislam <ixdell41@gmail.com>
2023-06-08 19:28:56 +0600

    table of users

--- src/Dashboard/AllUsers/AllUsers.jsx
+++ src/Dashboard/AllUsers/AllUsers.jsx
@@ -1,16 +1,64 @@
-import { useQueries } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 const AllUsers = () => {
-    const { data: users = [], refetch } = useQueries('users', async () => {
-        const res = await fetch('http://localhost:5000/users');
-        return res.json();
-    });
-    console.log(users)
-    return (
-        <div>
-            <h1>Total Users: {users?.length}</h1>
-        </div>
-    );
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
+    const res = await fetch("http://localhost:5000/users");
+    return res.json();
+  });
+
+  console.log(users);
+
+  return (
+    <div>
+      <h1>Total Users: {users?.length}</h1>
+      <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        users?.map((user, index) => <tr key={user._id}>
+            <td>{index+1}</td>
+            <td>
+              <div className="flex items-center space-x-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Hart Hagerty</div>
+                  <div className="text-sm opacity-50">United States</div>
+                </div>
+              </div>
+            </td>
+            <td>
+              Zemlak, Daniel and Leannon
+              <br/>
+              <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+            </td>
+            <td>Purple</td>
+            <th>
+              <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+          </tr>)
+      }
+      
+      
+    </tbody>
+    
+  </table>
+</div>
+    </div>
+  );
 };
 
 export default AllUsers;
