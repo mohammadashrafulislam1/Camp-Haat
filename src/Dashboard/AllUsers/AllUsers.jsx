3c56a5da20296b846330fdabb1716b92818da55e
mohammadashrafulislam <ixdell41@gmail.com>
2023-06-08 15:29:26 +0600

    some changes

--- src/Dashboard/AllUsers/AllUsers.jsx
+++ src/Dashboard/AllUsers/AllUsers.jsx
@@ -1,7 +1,14 @@
+import { useQueries } from "@tanstack/react-query";
+
 const AllUsers = () => {
+    const { data: users = [], refetch } = useQueries('users', async () => {
+        const res = await fetch('http://localhost:5000/users');
+        return res.json();
+    });
+    console.log(users)
     return (
         <div>
-            <h1>Total Users</h1>
+            <h1>Total Users: {users?.length}</h1>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,20 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Route.jsx';
 import AuthProvider from './Auth/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <AuthProvider>
+  <QueryClientProvider client={queryClient}>
   <RouterProvider router={router} />
+    </QueryClientProvider>
   </AuthProvider>
   </React.StrictMode>,
 )
