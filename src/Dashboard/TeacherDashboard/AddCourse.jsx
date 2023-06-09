31beca037cd18a0168eaa9c22a888e890ac3359d
mohammadashrafulislam <ixdell41@gmail.com>
2023-06-09 14:35:21 +0600

    jwt set

--- src/Dashboard/TeacherDashboard/AddCourse.jsx
+++ src/Dashboard/TeacherDashboard/AddCourse.jsx
@@ -1,8 +1,10 @@
 import { Helmet } from "react-helmet";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import useAuth from "../../hock/useAuth";
 
 const AddCourse = () => {
+    const {user} = useAuth();
     const {
         getValues,
         register,
@@ -10,80 +12,60 @@ const AddCourse = () => {
         formState: { errors },
       } = useForm();
       const onSubmit = (data) => {
-
+       console.log(data)
       }
     return (
-        <div className="w-full">
+        <div className="w-full border p-10 my-10 rounded-lg text-center">
         <Helmet>
             <title>Dashboard - Add Course</title>
         </Helmet>
           <form onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="text-3xl font-bold">Add A Course</h1>
           <div className="form-control">
               <label className="label">
-                <span className="label-text">Name <span className="text-red-600">*</span></span>
+                <span className="label-text">Course Name <span className="text-red-600">*</span></span>
               </label>
               <input type="text" placeholder="name" className="input input-bordered w-full" {...register("name", {required:{ value: true, message: "This field is required" }})}/>
               {errors.name && <span className="text-red-600 mt-2">{errors.name.message}</span>}
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Email <span className="text-red-600">*</span></span>
+                <span className="label-text">Instructor Name<span className="text-red-600">*</span></span>
               </label>
-              <input type="email" placeholder="email" className="input input-bordered w-full" {...register("email", {required:{ value: true, message: "This field is required" }})} />
-              {errors.email && <span className="text-red-600 mt-2">{errors.email.message}</span>}
+              <input type="text" placeholder="name" className="input input-bordered w-full" readOnly defaultValue={user?.displayName}/>
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Password <span className="text-red-600">*</span></span>
+                <span className="label-text">Instructor Email<span className="text-red-600">*</span></span>
               </label>
-              <input type="password" placeholder="password" className="input input-bordered w-full" {...register("password", {required:{ value: true, message: "This field is required" }, minLength: {
-                value: 6,
-                message: "must be 6 chars"
-              },
-              validate: (value) => {
-                return (
-                  [/[a-z]/, /[A-Z]/, /[0-9]/, /[^a-zA-Z0-9]/].every((pattern) =>
-                    pattern.test(value)
-                  ) || "must include lower, upper, number, and special chars"
-                );
-              },
-              })} />
-              {errors.password && <span className="text-red-600 mt-2">{errors.password.message}</span>}
+              <input type="text" placeholder="name" className="input input-bordered w-full" readOnly defaultValue={user?.email}/>
+            </div>
+        <div className="flex gap-10">
+        <div className="form-control">
+              <label className="label">
+                <span className="label-text">Available seats<span className="text-red-600">*</span></span>
+              </label>
+              <input type="number" placeholder="Available seats" className="input input-bordered w-full" {...register("seats", {required:{ value: true, message: "This field is required" }})} />
+              {errors.seats && <span className="text-red-600 mt-2">{errors.seats.message}</span>}
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Confirm Password <span className="text-red-600">*</span></span>
+                <span className="label-text">Price<span className="text-red-600">*</span></span>
               </label>
-              <input type="password" placeholder="Confirm Password" className="input input-bordered w-full" {...register("confirm", {required:{ value: true, message: "This field is required" },
-            validate: (value) => {
-                const { password } = getValues();
-                return password === value || "Passwords should match!";
-              },})}/>
-              {errors.confirm && <span className="text-red-600 mt-2">{errors.confirm.message}</span>}
+              <input type="number" placeholder="Price" className="input input-bordered w-full" {...register("price", {required:{ value: true, message: "This field is required" }})}/>
+              {errors.price && <span className="text-red-600 mt-2">{errors.price.message}</span>}
             </div>
-            
-            <div className="form-control">
-            <label className="label">
-                <span className="label-text">What is your role? <span className="text-red-600">*</span></span>
-              </label>
-            <select className="select select-bordered w-full max-w-xs" {...register("role", {required:{ value: true, message: "This field is required" }})} defaultValue='Your Role!'>
-            {errors.role && <span className="text-red-600 mt-2">{errors.role.message}</span>}
-            <option>Student</option>
-            <option>Instructor</option>
-             </select>
-             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Photo URL <span className="text-red-600">*</span></span>
+                <span className="label-text">Add a Photo<span className="text-red-600">*</span></span>
               </label>
-              <input type="text" placeholder="Photo URL" className="input input-bordered w-full" {...register("url", {required:{ value: true, message: "This field is required" }})}/>
-              {errors.url && <span className="text-red-600 mt-2">{errors.url.message}</span>}
+              <input type="file" className="file-input file-input-bordered file-input-primary w-full max-w-xs" {...register("file", {required:{ value: true, message: "This field is required" }})}/>
+              {errors.file && <span className="text-red-600 mt-2">{errors.file.message}</span>}
             </div>
-    
+        </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary w-full">Register</button>
+              <button className="btn btn-primary w-full">Add a course</button>
             </div>
-           <p>Already a member? <Link to='/login'>Login</Link></p>
           </form>
         </div>
     );
